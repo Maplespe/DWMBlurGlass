e4acede75bc930e8633f1cf242e82841cb5290d3
ALTaleX <2368730049@qq.com>
2024-05-02 13:34:18 +0800

    improve aero shader and fix config reading problems

--- DWMBlurGlassExt/Backdrops/BlurBackdrop.hpp
+++ DWMBlurGlassExt/Backdrops/BlurBackdrop.hpp
@@ -49,8 +49,153 @@ namespace MDWMBlurGlassExt::BlurBackdrop
 		compositeStepEffect->SetDestination(*gaussianBlurEffect);
 		compositeStepEffect->SetSource(*tintOpacityEffect);
 
-		auto effectBrush{ compositor.CreateEffectFactory(*compositeStepEffect).CreateBrush() };
-		effectBrush.SetSourceParameter(L"Backdrop", compositor.CreateBackdropBrush());
-		return effectBrush;
-	}
+			blurAmount = g_configData.customBlurAmount;
+		}
+		winrt::Windows::UI::Composition::CompositionBrush STDMETHODCALLTYPE GetBrush(bool useDarkMode, bool windowActivated) override try
+		{
+			auto is_device_valid = [&]()
+			{
+				if (!interopDCompDevice) return false;
+
+				BOOL valid{ FALSE };
+				THROW_IF_FAILED(
+					interopDCompDevice.as<IDCompositionDevice>()->CheckDeviceState(
+						&valid
+					)
+				);
+
+				return valid == TRUE;
+			};
+			if (!is_device_valid())
+			{
+				interopDCompDevice.copy_from(
+					DWM::CDesktopManager::s_pDesktopManagerInstance->GetDCompositionInteropDevice()
+				);
+				ReloadParameters();
+				auto compositor{ interopDCompDevice.as<winrt::Windows::UI::Composition::Compositor>() };
+
+				lightMode_Active_Brush = CreateBrush(
+					compositor,
+					lightMode_Active_Color,
+					lightMode_Active_TintOpacity,
+					blurAmount,
+					hostBackdrop
+				);
+				lightMode_Inactive_Brush = CreateBrush(
+					compositor,
+					lightMode_Inactive_Color,
+					lightMode_Inactive_TintOpacity,
+					blurAmount,
+					hostBackdrop
+				);
+				darkMode_Active_Brush = CreateBrush(
+					compositor,
+					darkMode_Active_Color,
+					darkMode_Active_TintOpacity,
+					blurAmount,
+					hostBackdrop
+				);
+				darkMode_Inactive_Brush = CreateBrush(
+					compositor,
+					darkMode_Inactive_Color,
+					darkMode_Inactive_TintOpacity,
+					blurAmount,
+					hostBackdrop
+				);
+			}
+
+			return CDCompResources::GetBrush(useDarkMode, windowActivated);
+		}
+		catch (...) { return nullptr; }
+	};
+
+	struct CBlurBackdrop : CDCompBackdrop
+	{
+		inline static CBlurResources s_sharedResources{};
+
+		STDMETHOD(UpdateColorizationColor)(
+			bool useDarkMode,
+			bool windowActivated
+			) override
+		{
+			if (useDarkMode)
+			{
+				if (windowActivated) { currentColor = s_sharedResources.darkMode_Active_Color; }
+				else { currentColor = s_sharedResources.darkMode_Inactive_Color; }
+			}
+			else
+			{
+				if (windowActivated) { currentColor = s_sharedResources.lightMode_Active_Color; }
+				else { currentColor = s_sharedResources.lightMode_Inactive_Color; }
+			}
+
+			return S_OK;
+		}
+		HRESULT STDMETHODCALLTYPE Update(
+			bool useDarkMode,
+			bool windowActivated
+		) try
+		{
+			THROW_IF_FAILED(UpdateColorizationColor(useDarkMode, windowActivated));
+			THROW_IF_FAILED(
+				TryCrossFadeToNewBrush(
+					spriteVisual.Compositor(),
+					s_sharedResources.GetBrush(useDarkMode, windowActivated),
+					s_sharedResources.crossfadeTime
+				)
+			);
+
+			return S_OK;
+		}
+		CATCH_RETURN()
+	};
+
+	struct CAccentBlurResources : CDCompResourcesBase
+	{
+
+	};
+	struct CAccentBlurBackdrop : CAccentDCompBackdrop
+	{
+		inline static CAccentBlurResources s_sharedResources{};
+		winrt::Windows::UI::Color currenTintColor{};
+
+		HRESULT STDMETHODCALLTYPE UpdateBrush(const DWM::ACCENT_POLICY& policy) try
+		{
+			s_sharedResources.ReloadParameters();
+			s_sharedResources.interopDCompDevice.copy_from(
+				DWM::CDesktopManager::s_pDesktopManagerInstance->GetDCompositionInteropDevice()
+			);
+			auto compositor{ spriteVisual.Compositor() };
+			auto tintColor{ FromAbgr(policy.nColor) };
+			if (
+				currenTintColor != tintColor ||
+				interopDCompDevice != s_sharedResources.interopDCompDevice
+			)
+			{
+				interopDCompDevice = s_sharedResources.interopDCompDevice;
+				currenTintColor = tintColor;
+
+				spriteVisual.Brush(
+					CBlurResources::CreateBrush(
+						compositor,
+						tintColor,
+						1.f,
+						CBlurBackdrop::s_sharedResources.blurAmount,
+						CBlurBackdrop::s_sharedResources.hostBackdrop
+					)
+				);
+			}
+
+			return S_OK;
+		}
+		CATCH_RETURN()
+
+		HRESULT STDMETHODCALLTYPE Update(const DWM::ACCENT_POLICY& policy) override try
+		{
+			THROW_IF_FAILED(UpdateBrush(policy));
+
+			return S_OK;
+		}
+		CATCH_RETURN()
+	};
 }
